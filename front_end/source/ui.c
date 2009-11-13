709b3ffb6ff5e6ae7fc9ad7602118a25ff540d6b
dariosus <dario15th@gmail.com>
2009-11-13 13:02:53 +0000

    Mi primer commit!

--- front_end/source/ui.c
+++ front_end/source/ui.c
@@ -10,15 +10,26 @@ void clearViewPort(){
 		printf("\n");
 }
 
-void drawTable( game_t * game ){
+void drawTable( game_t * game, int numModifiers, modifier_t modifiers[]  ){
 	int i, j;
 // 	static int colores[] = {GRIS,ROJO,AZUL_CLARO,VERDE,AMARILLO,CELESTE,ROSA,VIOLETA,MARRON,VERDE_CLARO};
 	backcolor(NEGRO);
 	textcolor(BLANCO);
+	//Modifiquemos la tabla de acuerdo a los modifiers! MENTIRA, NO ANDA
+	//for(i=0;i<numModifiers;i++){
+	//	textcolor( modifiers[i]->color );
+	//	game->players[ modifiers[i]-> nBoard ].board.matrix[ modifiers[i]-> x ][ modifiers[i]-> y ] = modifiers[i]->character;
+	//}
 	printf("\n");
 	printf(" ");
 	for( i=0 ; i < game->options.width ; i++ )
 		printf("+---");
+	//Si son 2 jugadores
+	if (game->options.mode == 2){
+		printf("+      ");
+		for( i=0 ; i < game->options.width ; i++ )
+		printf("+---");
+	}
 	printf("+\n");
 
 	for( i=0 ; i < game->options.height ; i++ ){
@@ -26,15 +37,35 @@ void drawTable( game_t * game ){
 
 			printf(" | ");
 
-// 			textcolor( colores[ (int)game->players[ game->state.next ].board.matrix[i][j] ] );
+ 			textcolor( colores[ (int)game->players[ game->state.next ].board.matrix[i][j] ] );
 
-// 			printf("%d", (int)game->players[ game->state.next ].board.matrix[i][j] );
+ 			printf("%d", (int)game->players[ game->state.next ].board.matrix[i][j] );
 
 			textcolor(BLANCO);
+		}
+		// si son 2 jugadores
+		if (game->options.mode == 2){
+			printf(" |      ");
+			for( j=0 ; j< game->options.width; j++ ){
+
+			printf(" | ");
+
+ 			textcolor( colores[ (int)game->players[ ((game->state.next)+1)% game->numplayers ].board.matrix[i][j] ] );
+
+ 			printf("%d", (int)game->players[ (game->state.next)+1)% game->numplayers ].board.matrix[i][j] );
+
+			textcolor(BLANCO);
+			}
 		}
 		printf(" |\n ");
 		for(j=0; j < game->options.width; j++)
 			printf("+---");
+		//Si son 2 jugadores
+		if (game->options.mode == 2){
+			printf("+      ");
+			for(j=0; j < game->options.width; j++)
+				printf("+---");
+		}
 		printf("+\n");
 	}
 	textattr(CLEAR);
