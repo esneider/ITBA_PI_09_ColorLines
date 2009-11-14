38d6b7fab6c6555a637f01c396d4d0fa356ddd43
esneider <dariosn@gmail.com>
2009-11-14 23:29:51 +0000

    playGame added

---
+++ back_end/include/playGame.h
@@ -0,0 +1,11 @@
+// playGame.h
+#ifndef PLAYGAME_H
+#define PLAYGAME_H
+
+#include <stdlib.h>
+#include "game.h"
+
+void randFill( game_t * game, int nPlayer, size_t cant, bool force );
+
+
+#endif // PLAYGAME_H
---
+++ back_end/source/playGame.c
@@ -0,0 +1,30 @@
+// playGame.c
+#include <stdbool.h>
+#include "error.h"
+#include "defines.h"
+#include "utils.h"
+#include "colors.h"
+#include "playGame.h"
+
+static bool notFree( game_t * game, int nPlayer, size_t x, size_t y, color c){
+	return false;
+}
+
+void randFill( game_t * game, int nPlayer, size_t cant, bool force ){
+	int i;
+	size_t x, y;
+	color c;
+
+	for( i = 0 ; i < cant ; i++ ){
+		if( game->players[nPlayer].board.emptySpots <= 0 )
+			break;
+		do{
+			x = rand() % game->options.width;
+			y = rand() % game->options.height;
+			c = rand() % game->options.numColors;
+		}while( game->players[nPlayer].board.matrix[y][x] != 0 ||
+				( force && notFree( game, nPlayer, x, y, c ) ) );
+		game->players[nPlayer].board.matrix[y][x] = c;
+		game->players[nPlayer].board.emptySpots--;
+	}
+}
